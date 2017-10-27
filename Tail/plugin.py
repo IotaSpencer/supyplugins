f6971984192f32c7355565f327f40c62089c1f2e
Ken Spencer <ken@electrocode.net>
2017-10-26 21:43:42 -0400

    Tail: fix how files are looked up

--- Tail/plugin.py
+++ Tail/plugin.py
@@ -69,7 +69,7 @@ class Tail(callbacks.Plugin):
 
     def _checkFiles(self):
         self.log.debug('Checking files.')
-        for filename in self.files.keys():
+        for filename in self.files:
             self.log.debug('Checking {} ...'.format(filename))
             self._checkFile(filename)
 
